cf162b8d54674112326e70babecf47c56aee09b2
Lio <lioit.npng@gmail.com>
2020-11-10 21:42:47 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://scontent-hkt1-2.xx.fbcdn.net/v/t1.0-9/123917425_2774667366147066_6204030706270332910_n.jpg?_nc_cat=108&ccb=2&_nc_sid=8bfeb9&_nc_ohc=ji_qH0srT4QAX-7uYxv&_nc_ht=scontent-hkt1-2.xx&oh=afbf4ca0a90e4575cc5b40a8f4b24909&oe=5FD18D2E" className="App-logo" alt="logo" />
         <p>
           I Love Duyen
         </p>
